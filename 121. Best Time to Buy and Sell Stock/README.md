67db1ed390cd05bfb6bc16a72cca958c54131840
esendjer <esendjer@gmail.com>
2023-07-18 17:37:24 +0300

    add solution for 121
    Signed-off-by: esendjer <esendjer@gmail.com>

--- 121. Best Time to Buy and Sell Stock/README.md
+++ 121. Best Time to Buy and Sell Stock/README.md
@@ -1,3 +1,5 @@
 # 121. Best Time to Buy and Sell Stock
 
 [121. Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/)
+
+Despite the `maxProfitBL` is a branchless solution in comparing with `maxProfit`, it works slowly
--- 121. Best Time to Buy and Sell Stock/python/best_time_to_buy_and_sell_stock.py
+++ 121. Best Time to Buy and Sell Stock/python/best_time_to_buy_and_sell_stock.py
@@ -5,11 +5,20 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_p = 0
         max_prof = 0
-        for i in range(len(prices)):
+        for i in range(1,len(prices)):
             if max_prof < prices[i] - prices[min_p]:
                 max_prof = prices[i] - prices[min_p]
             if prices[i] < prices[min_p]:
                 min_p = i
+        return max_prof
+
+    def maxProfitBL(self, prices: List[int]) -> int:
+        min_p = 0
+        max_prof = 0
+        for i in range(1,len(prices)):
+            pmi = prices[i] - prices[min_p]
+            max_prof = pmi * (max_prof < pmi) + max_prof * (max_prof >= pmi)
+            min_p = i * (prices[i] < prices[min_p]) + min_p * (prices[i] >= prices[min_p])
         return max_prof
 
 
@@ -19,6 +28,8 @@ cases = (
     [7,6,4,3,1],
     [2,4,1],
     [3,2,6,5,0,3],
+    [2,1,2,0,1],
+    [3,3,5,0,0,3,1,4],
 )
 
 a = Solution()
