cc0e096bcf3cc4fb6e1fdf97647f08a2890b7439
esendjer <esendjer@gmail.com>
2023-08-08 22:31:55 +0300

    434. Number of Segments in a String
    Signed-off-by: esendjer <esendjer@gmail.com>

--- 434. Number of Segments in a String/python/number_of_segments_in_a_string.py
+++ 434. Number of Segments in a String/python/number_of_segments_in_a_string.py
@@ -10,3 +10,13 @@ class Solution:
             if (i>0 and s[i-1] == " ") or (a == 0 and s[i] != " "):
                 a+=1
         return a
+    
+    def countSegmentsT(self, s: str) -> int:
+        a = 0
+        l = len(s)
+        for i in range(l):
+            if s[i] == " " and i and s[i-1] != " ":
+                a+=1
+            if i==l-1 and s[i] != " ":
+                a+=1
+        return a
