6109edf0da31fc8e9ffa54cc361b3354599bde9c
esendjer <esendjer@gmail.com>
2023-08-16 16:59:30 +0300

    commented 80
    Signed-off-by: esendjer <esendjer@gmail.com>

--- 80. Remove Duplicates from Sorted Array II/python/remove_duplicates_from_sorted_array_ii.py
+++ 80. Remove Duplicates from Sorted Array II/python/remove_duplicates_from_sorted_array_ii.py
@@ -3,9 +3,9 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-        c = 0
-        t = 1
+        i = 0  # current index
+        c = 0  # count of repetition
+        t = 1  # length without duplicates
         while i < len(nums)-1:
             if nums[i] == nums[i+1]:
                 if c:
